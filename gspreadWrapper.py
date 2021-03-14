c26698a28f09ba2aa34f3225273309389b4661e7
Lucio Baglione <coire1@gmail.com>
2021-03-14 22:08:55 +0100

    Add some utility methods:
    - countMarked: counts the occurrences of a "flag" column for a
    specific set of assessments.
    - groupByAssessor: group an assessments collection by assessor
    - createSheetFromGroup: create a new sheet in a spreadsheet from
    a list of assessments previously grouped by column.

--- gspreadWrapper.py
+++ gspreadWrapper.py
@@ -1,4 +1,6 @@
 import gspread
+from gspread.models import Cell
+from itertools import groupby
 
 from options import Options
 from utils import Utils
@@ -40,3 +42,57 @@ class GspreadWrapper():
                     data[self.options.assessmentsIdColumn] = i + 2
             return self.assessmentsData
         return False
+
+    def countMarked(self, data, column):
+        count = sum(map(lambda rec: rec[column] == 'x', data))
+        return count
+
+    def groupByAssessor(self, data):
+        assessors = {}
+        if (data):
+            sort = sorted(data, key=lambda rec: rec[self.options.assessorColumn])
+            for k, assessments in groupby(sort, lambda rec: rec[self.options.assessorColumn]):
+                assessments = list(assessments)
+                blankCount = self.countMarked(assessments, self.options.blankColumn)
+                assessors[k] = {}
+                assessors[k]['assessments'] = assessments
+                assessors[k]['total'] = len(assessments)
+                assessors[k]['profanity'] = self.countMarked(assessments, self.options.profanityColumn)
+                assessors[k]['blank'] = blankCount
+                assessors[k]['blankPercentage'] = (100 * blankCount) / len(assessments)
+                assessors[k]['score'] = self.countMarked(assessments, self.options.scoreColumn)
+                assessors[k]['copy'] = self.countMarked(assessments, self.options.copyColumn)
+                assessors[k]['wrongChallenge'] = self.countMarked(assessments, self.options.wrongChallengeColumn)
+                assessors[k]['wrongCriteria'] = self.countMarked(assessments, self.options.wrongCriteriaColumn)
+                assessors[k]['other'] = self.countMarked(assessments, self.options.otherColumn)
+        return assessors
+
+    def createSheetFromGroup(self, spreadsheet, title, data, keysWhitelist, columnsBlacklist):
+        # data is expected to be a dict of dict. A column for each dict key will
+        # be created
+        if (data):
+            headings = list(list(data.items())[0][1].keys())
+            # filtering out blacklisted keys
+            headings = [x for x in headings if (x not in columnsBlacklist)]
+            worksheet = spreadsheet.add_worksheet(title=title, rows=100, cols=len(headings) + 1)
+            cellsToAdd = []
+
+            for i, value in enumerate(headings):
+                cellsToAdd.append(
+                    Cell(row=1, col=(i + 2), value=value)
+                )
+
+            prIndex = 2
+            for index, mainKey in enumerate(data):
+                if (mainKey in keysWhitelist):
+                    cellsToAdd.append(
+                        Cell(row=prIndex, col=1, value=mainKey)
+                    )
+                    for i, k in enumerate(data[mainKey]):
+                        if (k not in columnsBlacklist):
+                            value = data[mainKey][k]
+                            cellsToAdd.append(
+                                Cell(row=prIndex, col=(i + 1), value=value)
+                            )
+                    prIndex = prIndex + 1
+            worksheet.update_cells(cellsToAdd, value_input_option='USER_ENTERED')
