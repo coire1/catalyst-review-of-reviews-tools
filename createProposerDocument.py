117565947607113ea565c8495d0dfc6501a7be9f
Lucio Baglione <coire1@gmail.com>
2021-05-08 16:20:59 +0200

    Update proposerDoc creation for dataframes.

--- createProposerDocument.py
+++ createProposerDocument.py
@@ -2,96 +2,74 @@ from gspreadWrapper import GspreadWrapper
 from options import Options
 from utils import Utils
 
-from gspread.models import Cell
 from gspread_formatting import *
+import pandas as pd
 
 class CreateProposerDocument():
     def __init__(self):
-        self.options = Options()
+        self.opt = Options()
         self.utils = Utils()
         self.gspreadWrapper = GspreadWrapper()
 
     def createDoc(self):
         print('Loading original...')
         self.gspreadWrapper.loadAssessmentsFile()
-        print('Make a new copy...')
-        spreadsheet = self.gspreadWrapper.gc.copy(
-            self.options.originalExportFromIdeascale,
-            title=self.options.proposerDocumentName
-        )
-        spreadsheet.share(
-            self.options.accountEmail,
-            perm_type='user',
-            role='writer'
+        self.gspreadWrapper.prepareDataFromExport()
+        spreadsheet, worksheet = self.gspreadWrapper.createDoc(
+            self.opt.proposerDocumentName,
+            "Assessments"
         )
 
-        worksheet = spreadsheet.worksheet(self.options.assessmentsSheet)
-
-        # Delete column for avg rating
-        worksheet.delete_columns(5)
-
-        print('Setting headings for report...')
-        # Add columns for y/r cards criteria
-        currentColsCount = worksheet.col_count - 1
-        cellsToAdd = []
-        # Set headings
+        # Define all the columns needed in the file
         headings = [
-            self.options.assessmentsIdColumn, self.options.tripletIdColumn,
-            self.options.blankColumn, self.options.topQualityColumn,
-            self.options.profanityColumn, self.options.scoreColumn,
-            self.options.copyColumn, self.options.wrongChallengeColumn,
-            self.options.wrongCriteriaColumn, self.options.otherColumn,
-            self.options.otherRationaleColumn
+            self.opt.proposalKeyCol, self.opt.ideaURLCol, self.opt.questionCol,
+            self.opt.assessorCol, self.opt.assessmentCol, self.opt.ratingCol,
+            self.opt.assessmentsIdCol, self.opt.tripletIdCol, self.opt.proposalIdCol,
+            self.opt.blankCol, self.opt.topQualityCol, self.opt.profanityCol,
+            self.opt.nonConstructiveCol, self.opt.scoreCol, self.opt.copyCol,
+            self.opt.incompleteReadingCol, self.opt.notRelatedCol,
+            self.opt.otherCol, self.opt.otherRationaleCol
         ]
-        worksheet.add_cols(len(headings))
 
-        print('Set column width...')
+        # Extract the columns already present in dataframe
+        existingHeadings = list(self.gspreadWrapper.df.columns)
+
+        # Create new Dataframe only with needed columns
+        proposerDf = pd.DataFrame()
+        for col in headings:
+            if (col in existingHeadings):
+                proposerDf[col] = self.gspreadWrapper.df[col]
+            else:
+                proposerDf[col] = ""
+
+        print('Assign blanks...')
+        # Assign 'x' for blank assessments
+        proposerDf[self.opt.blankCol] = proposerDf[self.opt.assessmentCol].apply(
+            lambda r: 'x' if (r.strip() == "") else ''
+        )
+
+        self.gspreadWrapper.writeDf(worksheet, proposerDf)
+
+        print('Format columns...')
         set_column_widths(worksheet, [
-            ('A:D', 150), ('E', 120), ('F', 400),
-            ('G', 60), ('H', 30), ('I', 60), ('J:Q', 30), ('R', 400)
+            ('A:D', 150), ('E', 400),
+            ('F', 60),('G:R', 30), ('S', 400)
         ])
 
-        for i, value in enumerate(headings):
-            cellsToAdd.append(
-                Cell(row=1, col=(currentColsCount + i + 1), value=value)
-            )
-
-        print('Mark blank assessments...')
-        # Autofill blank assessments
-        assessments = self.gspreadWrapper.getAssessmentsData()
-        for note in assessments:
-            col = (currentColsCount + 1)
-            cellsToAdd.append(
-                Cell(row=note[self.options.assessmentsIdColumn], col=col, value=note[self.options.assessmentsIdColumn])
-            )
-            col = (currentColsCount + 2)
-            cellsToAdd.append(
-                Cell(row=note[self.options.assessmentsIdColumn], col=col, value=note[self.options.tripletIdColumn])
-            )
-            assessment = note[self.options.assessmentColumn].strip()
-            if (assessment == ''):
-                col = (currentColsCount + 3)
-                cellsToAdd.append(
-                    Cell(row=note[self.options.assessmentsIdColumn], col=col, value='x')
-                )
-        worksheet.update_cells(cellsToAdd, value_input_option='USER_ENTERED')
         worksheet.freeze(rows=1)
 
         format_cell_ranges(worksheet, [
-            ('H:Q', self.utils.counterFormat),
-            ('A1:G1', self.utils.headingFormat),
-            ('R1', self.utils.headingFormat),
-            ('G1:Q1', self.utils.verticalHeadingFormat),
+            ('F:R', self.utils.counterFormat),
+            ('A1:S1', self.utils.headingFormat),
+            ('F1:R1', self.utils.verticalHeadingFormat),
             ('K2:K', self.utils.greenFormat),
             ('L2:L', self.utils.redFormat),
-            ('M2:Q', self.utils.yellowFormat),
-            ('A2:G', self.utils.textFormat),
+            ('M2:R', self.utils.yellowFormat),
+            ('A2:E', self.utils.textFormat),
 
         ])
         print('Document for proposers created')
         print('Link: {}'.format(spreadsheet.url))
-
-
 
 c = CreateProposerDocument()
 c.createDoc()
--- gspreadWrapper.py
+++ gspreadWrapper.py
@@ -5,254 +5,53 @@ from gspread.utils import (
     finditem,
     fill_gaps
 )
-from itertools import groupby
 
 from options import Options
 from utils import Utils
+import pandas as pd
 
-
-'''
-    Monkey patch to get merged cells values.
-'''
-
-def fix_merge_values(worksheet_metadata, values):
-    """Assign the top-left value to all cells in a merged range."""
-    for merge in worksheet_metadata.get("merges", []):
-        start_row, end_row = merge["startRowIndex"], merge["endRowIndex"]
-        start_col, end_col = (merge["startColumnIndex"], merge["endColumnIndex"])
-
-        # ignore merge cells outside the data range
-        if start_row < len(values) and start_col < len(values[0]):
-            orig_val = values[start_row][start_col]
-            for row in values[start_row:end_row]:
-                row[start_col:end_col] = [
-                    orig_val for i in range(start_col, end_col)
-                ]
-
-    return values
-
-def _get_all_values(self, value_render_option='FORMATTED_VALUE'):
-    """Returns a list of lists containing all cells' values as strings.
-    :param value_render_option: (optional) Determines how values should be
-                                rendered in the the output. See
-                                `ValueRenderOption`_ in the Sheets API.
-    :type value_render_option: str
-    .. _ValueRenderOption: https://developers.google.com/sheets/api/reference/rest/v4/ValueRenderOption
-    .. note::
-        Empty trailing rows and columns will not be included.
-    """
-    title = self.title.replace("'", "''")
-    data = self.spreadsheet.values_get(
-        "'{}'".format(title),
-        params={'valueRenderOption': value_render_option}
-    )
-    spreadsheet_meta = self.spreadsheet.fetch_sheet_metadata()
-
-    # not catching StopIteration becuase Worksheet exists
-    # potential issue if this Worksheet
-    worksheet_meta = finditem(
-        lambda x: x['properties']['title'] == self.title,
-        spreadsheet_meta['sheets']
-    )
-
-    try:
-        values = fill_gaps(data['values'])
-        return fix_merge_values(worksheet_meta, values)
-    except KeyError:
-        return []
-# Apply Monkey patch
-gspread.models.Worksheet.get_all_values = _get_all_values
 
 class GspreadWrapper():
     def __init__(self):
-        self.options = Options()
+        self.opt = Options()
         self.utils = Utils()
-        self.gc = gspread.service_account(filename=self.options.gSheetAuthFile)
-        self.assessmentsData = {}
+        self.gc = gspread.service_account(filename=self.opt.gSheetAuthFile)
 
     def loadAssessmentsFile(self):
-        self.assessmentsDoc = self.gc.open_by_key(self.options.originalExportFromIdeascale)
-        self.assessmentsSheet = self.assessmentsDoc.worksheet(self.options.assessmentsSheet)
+        self.assessmentsDoc = self.gc.open_by_key(self.opt.originalExportFromIdeascale)
+        self.assessmentsSheet = self.assessmentsDoc.worksheet(self.opt.assessmentsSheet)
+        self.df = False
 
-    def getProposersData(self):
-        self.proposersDoc = self.gc.open_by_key(self.options.proposersFile)
-        self.proposersSheet = self.proposersDoc.worksheet(self.options.assessmentsSheet)
-        self.proposersData = self.proposersSheet.get_all_records()
-        proposalsWithIds = self.generateProposalsId(self.proposersData)
-        for (i, data) in enumerate(self.proposersData):
-            # Add id if not present. 2 Offset because Google Sheet starts from 1
-            # and the first row is the table heading.
-            if (self.options.assessmentsIdColumn not in data):
-                data[self.options.assessmentsIdColumn] = i + 2
-            if (self.options.tripletIdColumn not in data):
-                assessorId = data[self.options.assessorColumn].replace('z_assessor_', '')
-                proposalK = data[self.options.proposalKeyColumn]
-                proposalId = proposalsWithIds[proposalK]
-                data[self.options.tripletIdColumn] = "{}-{}".format(assessorId, proposalId)
-        return self.proposersData
-
-    def getAssessmentsData(self):
+    def prepareDataFromExport(self):
         if (self.assessmentsSheet):
-            cacheFn = self.options.assessmentsCacheFilename
-            self.assessmentsData = self.assessmentsSheet.get_all_records()
-            # Loop over all records to assign a unique id
-            proposalsWithIds = self.generateProposalsId(self.assessmentsData)
-            for (i, data) in enumerate(self.assessmentsData):
-                # Add id if not present. 2 Offset because Google Sheet starts from 1
-                # and the first row is the table heading.
-                if (self.options.assessmentsIdColumn not in data):
-                    data[self.options.assessmentsIdColumn] = i + 2
-                if (self.options.tripletIdColumn not in data):
-                    assessorId = data[self.options.assessorColumn].replace('z_assessor_', '')
-                    proposalK = data[self.options.proposalKeyColumn]
-                    proposalId = proposalsWithIds[proposalK]
-                    data[self.options.tripletIdColumn] = "{}-{}".format(assessorId, proposalId)
-            return self.assessmentsData
-        return False
+            self.df = pd.DataFrame(self.assessmentsSheet.get_all_records())
+            # Assign ids in new column
+            self.df.insert(0, self.opt.assessmentsIdCol, self.df.index + 1)
+            # Assign proposal ids
+            self.df[self.opt.proposalIdCol] = self.df.groupby(
+                self.opt.proposalKeyCol
+            ).ngroup()
+            # Assign assessor_id
+            self.df['assessor_id'] = self.df[self.opt.assessorCol].str.replace('z_assessor_', '')
+            # Assign triplet_id
+            self.df[self.opt.tripletIdCol] = self.df['assessor_id'] + '-' + self.df[self.opt.proposalIdCol].astype(str)
 
-    def countMarked(self, data, column):
-        count = sum(map(lambda rec: rec[column].strip() != '', data))
-        return count
+    def createDoc(self, name, sheetName):
+        print('Create new document...')
+        spreadsheet = self.gc.create(name)
+        spreadsheet.share(
+            self.opt.accountEmail,
+            perm_type='user',
+            role='writer'
+        )
 
-    def generateProposalsId(self, data):
-        proposals = {}
-        if (data):
-            sort = sorted(data, key=lambda rec: rec[self.options.proposalKeyColumn])
-            proposalId = 1
-            for k, v in groupby(sort, lambda rec: rec[self.options.proposalKeyColumn]):
-                proposals[k] = proposalId
-                proposalId = proposalId + 1
+        print('Create sheet...')
+        worksheet = spreadsheet.get_worksheet(0)
+        worksheet.update_title(sheetName)
 
-        return proposals
+        return spreadsheet, worksheet
 
-
-    def groupByAssessorBlank(self, key):
-        manualRemovedRecapDoc = self.gc.open_by_key(key)
-        manualBlankAssessorsSheet = manualRemovedRecapDoc.worksheet('Assessors')
-        data = manualBlankAssessorsSheet.get_all_records()
-        assessors = {}
-        if (data):
-            for row in data:
-                assessors[row[self.options.assessorColumn]] = row['manualBlanks']
-        return assessors
-
-    def groupByAssessor(self, data, manualBlanksAssessors):
-        assessors = {}
-        if (data):
-            sort = sorted(data, key=lambda rec: rec[self.options.assessorColumn])
-            for k, assessments in groupby(sort, lambda rec: rec[self.options.assessorColumn]):
-                assessments = list(assessments)
-                blankCount = self.countMarked(assessments, self.options.blankColumn)
-                manualBlankCount = 0
-                if (k in manualBlanksAssessors):
-                    manualBlankCount = manualBlanksAssessors[k]
-                totalBlank = blankCount + manualBlankCount
-                total = len(assessments) + manualBlankCount
-                assessors[k] = {}
-                assessors[k]['assessments'] = assessments
-                assessors[k]['total'] = total
-                assessors[k]['profanity'] = self.countMarked(assessments, self.options.profanityColumn)
-                assessors[k]['manualBlanks'] = manualBlankCount
-                assessors[k]['blank'] = blankCount
-                assessors[k]['blankPercentage'] = totalBlank / total
-                assessors[k]['score'] = self.countMarked(assessments, self.options.scoreColumn)
-                assessors[k]['copy'] = self.countMarked(assessments, self.options.copyColumn)
-                assessors[k]['wrongChallenge'] = self.countMarked(assessments, self.options.wrongChallengeColumn)
-                assessors[k]['wrongCriteria'] = self.countMarked(assessments, self.options.wrongCriteriaColumn)
-                assessors[k]['other'] = self.countMarked(assessments, self.options.otherColumn)
-        return assessors
-
-    def groupById(self, data):
-        ids = {}
-        if (data):
-            for row in data:
-                ids[row[self.options.assessmentsIdColumn]] = row
-        return ids
-
-    def createSheetFromGroup(
-        self,
-        spreadsheet,
-        title,
-        data,
-        keysWhitelist,
-        columnsBlacklist,
-        columnWidths=False,
-        formats=False
-    ):
-        # data is expected to be a dict of dict. A column for each dict key will
-        # be created
-        if (data):
-            headings = list(list(data.items())[0][1].keys())
-            # filtering out blacklisted keys
-            headings = [x for x in headings if (x not in columnsBlacklist)]
-            worksheet = spreadsheet.add_worksheet(title=title, rows=100, cols=len(headings) + 1)
-            cellsToAdd = []
-
-            if (columnWidths is not False):
-                set_column_widths(worksheet, columnWidths)
-            if (formats is not False):
-                format_cell_ranges(worksheet, formats)
-
-            for i, value in enumerate(headings):
-                cellsToAdd.append(
-                    Cell(row=1, col=(i + 2), value=value)
-                )
-
-            prIndex = 2
-            for index, mainKey in enumerate(data):
-                if (mainKey in keysWhitelist):
-                    cellsToAdd.append(
-                        Cell(row=prIndex, col=1, value=mainKey)
-                    )
-                    for i, k in enumerate(data[mainKey]):
-                        if (k not in columnsBlacklist):
-                            value = data[mainKey][k]
-                            cellsToAdd.append(
-                                Cell(row=prIndex, col=(i + 1), value=value)
-                            )
-                    prIndex = prIndex + 1
-            worksheet.update_cells(cellsToAdd, value_input_option='USER_ENTERED')
-            worksheet.freeze(rows=1)
-
-
-    def createSheetFromList(
-        self,
-        spreadsheet,
-        title,
-        data,
-        columnsBlacklist,
-        columnWidths=False,
-        formats=False
-    ):
-        # data is expected to be a list of dict. A column for each dict key will
-        # be created
-        if (data):
-            headings = list(data[0].keys())
-            # filtering out blacklisted keys
-            headings = [x for x in headings if (x not in columnsBlacklist)]
-            worksheet = spreadsheet.add_worksheet(title=title, rows=100, cols=len(headings) + 1)
-
-            if (columnWidths is not False):
-                set_column_widths(worksheet, columnWidths)
-            if (formats is not False):
-                format_cell_ranges(worksheet, formats)
-
-            cellsToAdd = []
-            for i, value in enumerate(headings):
-                cellsToAdd.append(
-                    Cell(row=1, col=(i + 1), value=value)
-                )
-
-            # Add aggregated counts to cells
-            prIndex = 2
-            for el in data:
-                for j, k in enumerate(el):
-                    if (k not in columnsBlacklist):
-                        v = el[k]
-                        cellsToAdd.append(
-                            Cell(row=prIndex, col=(j + 1), value=v)
-                        )
-                prIndex = prIndex + 1
-
-            worksheet.update_cells(cellsToAdd, value_input_option='USER_ENTERED')
-            worksheet.freeze(rows=1)
+    def writeDf(self, worksheet, df):
+        worksheet.update(
+            [df.columns.values.tolist()] + df.values.tolist()
+        )
