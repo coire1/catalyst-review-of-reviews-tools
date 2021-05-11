0490847ccbc80757692f6a816da445c3890f3d19
Lucio Baglione <coire1@gmail.com>
2021-05-11 21:01:24 +0200

    Update proposer script to use dataframe utils.

--- createProposerDocument.py
+++ createProposerDocument.py
@@ -14,10 +14,9 @@ class CreateProposerDocument():
     def createDoc(self):
         print('Loading original...')
         self.gspreadWrapper.loadAssessmentsFile()
-        self.gspreadWrapper.prepareDataFromExport()
-        spreadsheet, worksheet = self.gspreadWrapper.createDoc(
-            self.opt.proposerDocumentName,
-            "Assessments"
+        proposerDf = self.gspreadWrapper.prepareDataFromExport()
+        spreadsheet = self.gspreadWrapper.createDoc(
+            self.opt.proposerDocumentName
         )
 
         # Define all the columns needed in the file
@@ -31,34 +30,19 @@ class CreateProposerDocument():
             self.opt.otherCol, self.opt.otherRationaleCol
         ]
 
-        # Extract the columns already present in dataframe
-        existingHeadings = list(self.gspreadWrapper.df.columns)
-
-        # Create new Dataframe only with needed columns
-        proposerDf = pd.DataFrame()
-        for col in headings:
-            if (col in existingHeadings):
-                proposerDf[col] = self.gspreadWrapper.df[col]
-            else:
-                proposerDf[col] = ""
-
         print('Assign blanks...')
         # Assign 'x' for blank assessments
         proposerDf[self.opt.blankCol] = proposerDf[self.opt.assessmentCol].apply(
             lambda r: 'x' if (r.strip() == "") else ''
         )
 
-        self.gspreadWrapper.writeDf(worksheet, proposerDf)
-
         print('Format columns...')
-        set_column_widths(worksheet, [
+        widths = [
             ('A:D', 150), ('E', 400),
             ('F', 60),('G:R', 30), ('S', 400)
-        ])
+        ]
 
-        worksheet.freeze(rows=1)
-
-        format_cell_ranges(worksheet, [
+        formats = [
             ('F:R', self.utils.counterFormat),
             ('A1:S1', self.utils.headingFormat),
             ('F1:R1', self.utils.verticalHeadingFormat),
@@ -66,8 +50,16 @@ class CreateProposerDocument():
             ('L2:L', self.utils.redFormat),
             ('M2:R', self.utils.yellowFormat),
             ('A2:E', self.utils.textFormat),
+        ]
 
-        ])
+        self.gspreadWrapper.createSheetFromDf(
+            spreadsheet,
+            "Assessments",
+            proposerDf,
+            headings,
+            widths,
+            formats
+        )
         print('Document for proposers created')
         print('Link: {}'.format(spreadsheet.url))
 
