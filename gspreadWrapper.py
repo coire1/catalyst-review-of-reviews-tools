589aec9d8787c095eb8dfd2f0a4121a2e6988a00
Lucio Baglione <coire1@gmail.com>
2021-05-11 21:00:21 +0200

    Add some utility functions to interact with dataframes.

--- gspreadWrapper.py
+++ gspreadWrapper.py
@@ -17,6 +17,12 @@ class GspreadWrapper():
         self.utils = Utils()
         self.gc = gspread.service_account(filename=self.opt.gSheetAuthFile)
 
+        self.infringementsColumns = [
+            self.opt.profanityCol, self.opt.nonConstructiveCol,
+            self.opt.scoreCol, self.opt.copyCol, self.opt.incompleteReadingCol,
+            self.opt.notRelatedCol, self.opt.otherCol
+        ]
+
     def loadAssessmentsFile(self):
         self.assessmentsDoc = self.gc.open_by_key(self.opt.originalExportFromIdeascale)
         self.assessmentsSheet = self.assessmentsDoc.worksheet(self.opt.assessmentsSheet)
@@ -35,8 +41,30 @@ class GspreadWrapper():
             self.df['assessor_id'] = self.df[self.opt.assessorCol].str.replace('z_assessor_', '')
             # Assign triplet_id
             self.df[self.opt.tripletIdCol] = self.df['assessor_id'] + '-' + self.df[self.opt.proposalIdCol].astype(str)
+            return self.df
+        return False
 
-    def createDoc(self, name, sheetName):
+    def getProposersData(self):
+        self.proposersDoc = self.gc.open_by_key(self.opt.proposersFile)
+        self.proposersSheet = self.proposersDoc.worksheet(self.opt.assessmentsSheet)
+        self.dfProposers = pd.DataFrame(self.proposersSheet.get_all_records())
+        self.dfProposers[self.opt.proposerMarkCol] = self.dfProposers.apply(self.checkMarks, axis=1)
+
+    def checkMarks(self, row):
+        res = False
+        for col in self.infringementsColumns:
+            if (row[col].strip() != ''):
+                # If General check also the rationale
+                if (col == self.opt.otherCol):
+                    if (row[self.opt.otherRationaleCol].strip() != ''):
+                        res = True
+                else:
+                    res = True
+        if (row[self.opt.topQualityCol].strip() != ''):
+            res = (not res)
+        return res
+
+    def createDoc(self, name):
         print('Create new document...')
         spreadsheet = self.gc.create(name)
         spreadsheet.share(
@@ -45,13 +73,35 @@ class GspreadWrapper():
             role='writer'
         )
 
-        print('Create sheet...')
-        worksheet = spreadsheet.get_worksheet(0)
-        worksheet.update_title(sheetName)
+        #blank_worksheet = spreadsheet.get_worksheet(0)
+        #spreadsheet.del_worksheet(blank_worksheet)
 
-        return spreadsheet, worksheet
+        return spreadsheet
 
-    def writeDf(self, worksheet, df):
+    def writeDf(self, worksheet, df, headings=False):
+        # Extract the columns already present in dataframe
+        existingHeadings = list(df.columns)
+        if headings == False:
+            headings = existingHeadings
+        # Create new Dataframe only with needed columns
+        newDf = pd.DataFrame()
+        for col in headings:
+            if (col in existingHeadings):
+                newDf[col] = df[col]
+            else:
+                newDf[col] = ""
+
         worksheet.update(
-            [df.columns.values.tolist()] + df.values.tolist()
+            [newDf.columns.values.tolist()] + newDf.values.tolist()
         )
+
+    def createSheetFromDf(self, spreadsheet, sheetName, df, headings=False, columnWidths=False, formats=False):
+        print('Create sheet...')
+        worksheet = spreadsheet.add_worksheet(title=sheetName, rows=1, cols=1)
+        self.writeDf(worksheet, df, headings)
+        if (columnWidths is not False):
+            set_column_widths(worksheet, columnWidths)
+        if (formats is not False):
+            format_cell_ranges(worksheet, formats)
+
+        worksheet.freeze(rows=1)
