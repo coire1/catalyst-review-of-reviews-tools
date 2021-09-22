64149623071feef6fcbc2168a9a051534a3a4412
Lucio Baglione <coire1@gmail.com>
2021-09-22 15:52:03 +0200

    Add some log to downloader script.

--- downloadCsvFromDrive.py
+++ downloadCsvFromDrive.py
@@ -1,21 +1,31 @@
 from gspreadWrapper import GspreadWrapper
 import pandas as pd
+import gspread
 
 class DownloadCsvFromDrive():
     def __init__(self):
         self.gspreadWrapper = GspreadWrapper()
         self.fileList = [
-            "18D79kcl3jXkuCF43jtBdeiz59IXFkb_vvG8fBmzzq84"
         ]
 
     def downloadFiles(self):
         for gfile in self.fileList:
-            doc = self.gspreadWrapper.gc.open_by_key(gfile)
-            # doc = self.gspreadWrapper.gc.open_by_url(gfile)
-            sheet = doc.worksheet("Assessments")
-            df = pd.DataFrame(sheet.get_all_records())
-            df.fillna('', inplace=True)
-            df.to_csv('proposers-files/' + doc.title + '.csv', index=False)
+            try:
+                print("\n######\n")
+                print("Downloading: {}".format(gfile))
+                doc = self.gspreadWrapper.gc.open_by_key(gfile)
+                # doc = self.gspreadWrapper.gc.open_by_url(gfile)
+                sheet = doc.worksheet("Assessments")
+                df = pd.DataFrame(sheet.get_all_records())
+                df.fillna('', inplace=True)
+                df.to_csv('proposers-files/' + doc.title + '.csv', index=False)
+                print("Downloaded successfully: {}".format(gfile))
+            except gspread.exceptions.APIError as e:
+                print("GDrive error downloading: {}".format(gfile))
+                print(e)
+            except:
+                print("Generic error downloading: {}".format(gfile))
+
 
 down = DownloadCsvFromDrive()
 down.downloadFiles()
