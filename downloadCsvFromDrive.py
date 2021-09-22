c94ce66138e1794584c5573bcc039da138874a93
Lucio Baglione <coire1@gmail.com>
2021-09-22 19:54:36 +0200

    Add log for error.

--- downloadCsvFromDrive.py
+++ downloadCsvFromDrive.py
@@ -1,12 +1,15 @@
 from gspreadWrapper import GspreadWrapper
 import pandas as pd
 import gspread
+import json
 
 class DownloadCsvFromDrive():
     def __init__(self):
         self.gspreadWrapper = GspreadWrapper()
         self.fileList = [
+            "1LA4Gt1W1PU-tMwFoef7DbtUWLeo3W0jD4HqEDCX46wM"
         ]
+        self.fileErrors = []
 
     def downloadFiles(self):
         for gfile in self.fileList:
@@ -22,10 +25,15 @@ class DownloadCsvFromDrive():
                 print("Downloaded successfully: {}".format(gfile))
             except gspread.exceptions.APIError as e:
                 print("GDrive error downloading: {}".format(gfile))
+                self.fileErrors.append(gfile)
                 print(e)
             except:
+                print(e)
+                self.fileErrors.append(gfile)
                 print("Generic error downloading: {}".format(gfile))
 
+        with open('download-errors.json', 'w') as f:
+            json.dump(self.fileErrors, f)
 
 down = DownloadCsvFromDrive()
 down.downloadFiles()
