78f56edea80a2ce61b8a718a7ab1dece7e2f6c79
Lucio Baglione <coire1@gmail.com>
2022-01-06 17:36:12 +0100

    Improve downloadCsv to better handle exceptions.

--- downloadCsvFromDrive.py
+++ downloadCsvFromDrive.py
@@ -4,18 +4,49 @@ import pandas as pd
 import gspread
 import json
 import re
+from time import sleep
 
 class DownloadCsvFromDrive():
     def __init__(self):
         self.gspreadWrapper = GspreadWrapper()
-        self.fileList = [
-        ]
+        self.fileList = []
+
         self.fileErrors = []
 
     def get_valid_filename(self, name):
         s = str(name).strip().replace(' ', '_')
         s = re.sub(r'(?u)[^-\w.]', '', s)
         return s
+
+    def download_from_sheet(self, gfile, docId):
+        try:
+            doc = self.gspreadWrapper.gc.open_by_key(docId)
+            sheets = doc.worksheets()
+            sheetsTitles = [x.title for x in sheets]
+            if ('Assessments' in sheetsTitles):
+                sheet = doc.worksheet("Assessments")
+            else:
+                sheet = doc.get_worksheet(0)
+            df = pd.DataFrame(sheet.get_all_records())
+            df.fillna('', inplace=True)
+            df.to_csv('proposers-files/' + self.get_valid_filename(doc.title) + '.csv', index=False)
+            print("Downloaded successfully: {}".format(gfile))
+        except gspread.exceptions.APIError as e:
+            dict_error = e.response.json()
+            if dict_error['error']['status'] == 'RESOURCE_EXHAUSTED':
+                print("Rate limit exceeded. Sleep for 30s...")
+                sleep(30)
+                print("Retry download...")
+                self.download_from_sheet(gfile, docId)
+            else:
+                print("GSheet error downloading: {}".format(gfile))
+                self.fileErrors.append(gfile)
+                print(e)
+        except Exception as e:
+            print(e)
+            self.fileErrors.append(gfile)
+            print("Generic error downloading: {}".format(gfile))
+
 
     def downloadFiles(self):
         for gfile in self.fileList:
@@ -28,26 +59,7 @@ class DownloadCsvFromDrive():
                         durl = 'https://drive.google.com/uc?id=' + docId
                         gdown.download(durl, 'proposers-files/' + docId + '.csv', quiet=True)
                     else:
-                        try:
-                            doc = self.gspreadWrapper.gc.open_by_key(docId)
-                            sheets = doc.worksheets()
-                            sheetsTitles = [x.title for x in sheets]
-                            if ('Assessments' in sheetsTitles):
-                                sheet = doc.worksheet("Assessments")
-                            else:
-                                sheet = doc.get_worksheet(0)
-                            df = pd.DataFrame(sheet.get_all_records())
-                            df.fillna('', inplace=True)
-                            df.to_csv('proposers-files/' + self.get_valid_filename(doc.title) + '.csv', index=False)
-                            print("Downloaded successfully: {}".format(gfile))
-                        except gspread.exceptions.APIError as e:
-                            print("GSheet error downloading: {}".format(gfile))
-                            self.fileErrors.append(gfile)
-                            print(e)
-                        except Exception as e:
-                            print(e)
-                            self.fileErrors.append(gfile)
-                            print("Generic error downloading: {}".format(gfile))
+                        self.download_from_sheet(gfile, docId)
                 else:
                     print("Not valid Google doc/sheet found.")
 
