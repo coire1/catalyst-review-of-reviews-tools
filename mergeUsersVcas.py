3e81710924599eafdcfec0da830087c5e1fbd6d2
Lucio Baglione <coire1@gmail.com>
2022-01-07 12:27:12 +0100

    Add script to merge info about proposers from users.json to vca.json.

---
+++ mergeUsersVcas.py
@@ -0,0 +1,12 @@
+import json
+
+users = json.load(open('users.json'))
+vcas = json.load(open('vcas.json'))
+for vca in vcas:
+    related_ca = next((item for item in users if item['id'] == vca['ca_id']), None)
+    if (related_ca):
+        vca['proposals'] = related_ca['proposals']
+        vca['campaigns_as_proposers'] = related_ca['campaigns']
+
+with open('vcas.json', 'w') as f:
+    json.dump(vcas, f, indent=4)
